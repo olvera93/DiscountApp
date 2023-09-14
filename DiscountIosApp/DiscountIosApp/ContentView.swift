1d59597898475f9c3424c05118d7d14b5c1c8db0
olvera93 <olvera9302@outlook.com>
2023-09-14 11:57:28 -0600

    first commit

---
+++ DiscountIosApp/DiscountIosApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiscountIosApp
+//
+//  Created by Gonzalo Olvera Monroy on 14/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world! Mother fuckers")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiscountIosApp/DiscountIosApp/DiscountIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscountIosAppApp.swift
+//  DiscountIosApp
+//
+//  Created by Gonzalo Olvera Monroy on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DiscountIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
